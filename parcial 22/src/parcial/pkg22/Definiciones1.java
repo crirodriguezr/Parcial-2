bf6ad6a6cb3b637e3b767adc81b737656af7845d
Estudiantes <Estudiantes@10.20.151.149>
2018-05-25 11:57:43 -0500

    Cans Wars

---
+++ parcial 22/src/parcial/pkg22/Definiciones1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial.pkg22;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Definiciones1 {
+  /**
+    Parcial numero 2 programación orientada a objetos.
+
+1.       Defina
+
+-Clase: La definición de clase  se refiere a lo que denominamos como relacionar datos , funciones, métodos, comportamientos y encapsularlos por medio de un objeto, el cual a partir de esta, obtendrá dichas características declaradas primeramente en la clase.
+
+-Objeto: El objeto puede ser considerado como el paradigma de POO y se refiere a la unidad, que puede heredar datos y comportamientos encapsulados en el interior de una clase, estos generalmente intercambian mensajes entre si teniendo en cuenta dicho comportamiento declarado en las funciones.
+
+-Clase abstracta: La clase abstracta, es dicha clase la cual declara métodos, sin embargo, no implementa al menos uno de ellos, esto requiere en todos los casos de un pilar de la Programación orientada a objetos llamada herencia con el fin de crear objetos que intercambian mensajes entre sí.
+
+-Interfaz: La interfaz es un tipo de clase la cual usa también uno de los pilares de la programación orientada a objetos, la cual es la herencia. Ya que esta declara todos sus métodos pero a diferencia de la clase abstracta, esta no implementa ni uno de aquellos métodos, por lo tanto lo que se genera es un contrato donde la interfaz dice que se tiene que elaborar dicho resultado, sin importarle con que procedimiento se hará o que herramientas usara.
+
+-Herencia: Pilar de la programación Orientada a objetos donde por medio de una jerarquía de objetos se genera una reutilización de código, incluyendo lo que definimos como datos y funciones.
+
+-Polimorfismo: Polimorfismo como unidad, se trata de otro pilar de la programación orientada a objetos donde se declaran objetos que responden a un mismo comportamiento o método, teniendo en cuenta que estos de entrada solicitan diferentes parámetros, como puede que algunos no soliciten de estos.
+  */
+}
