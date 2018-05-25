68505835efdcb0d63bae32bfb94b044fc931bb9a
Estudiantes <Estudiantes@10.20.151.149>
2018-05-25 11:53:53 -0500

    Cans Wars

---
+++ parcial 22/src/parcial/pkg22/Calculador.java
@@ -0,0 +1,40 @@
+
+package parcial.pkg22;
+
+
+
+public class Calculador {
+  
+       int xp1;
+        int xp2;
+       int yp1;
+        int yp2;
+        
+        float calcularDistancia(){
+          int distancia = 0;
+          int difx = 0;
+          int dify;
+          int difx2;
+          int dify2;
+          int raizd;
+          
+          xp2-xp1=difx;
+          yp2-yp1=dify;
+          
+         Math.pow(difx,2)=difx2;
+         Mth.pow(dify,2)=dify2;
+          
+          difx2+dify2=raizd;
+          Math
+          
+          
+          
+            return distancia;
+          
+        }
+        
+        
+        
+        
+        
+}
